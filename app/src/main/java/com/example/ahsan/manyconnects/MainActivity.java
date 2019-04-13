64e49f075ad9cddbbea1eba1b2aeb0e4c638393c
AhsanFaheem <ahsanfaheem1234@gmail.com>
2019-04-13 21:28:04 +0500

    second commit

--- app/src/main/java/com/example/ahsan/manyconnects/MainActivity.java
+++ app/src/main/java/com/example/ahsan/manyconnects/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ahsan.manyconnects;
-
 import android.content.Intent;
+import android.se.omapi.Session;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.FacebookActivity;
+import com.facebook.FacebookContentProvider;
+import com.facebook.FacebookSdk;
+import com.facebook.internal.FacebookInitProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(MainActivity.this,NewMsg.class);
-        startActivity(intent);
+       /* Intent intent=new Intent(MainActivity.this,NewMsg.class);
+        startActivity(intent);*/
 
     }
 }
--- app/src/main/java/com/example/ahsan/manyconnects/NewMsg.java
+++ app/src/main/java/com/example/ahsan/manyconnects/NewMsg.java
@@ -1,8 +1,17 @@
 package com.example.ahsan.manyconnects;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class NewMsg extends AppCompatActivity {
 
@@ -10,5 +19,22 @@ public class NewMsg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_msg);
+        printHashKey(this);
+    }
+
+    public void printHashKey(Context pContext) {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i("RESPONSE=", "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("RESPONSE=", "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e("RESPONSE=", "printHashKey()", e);
+        }
     }
 }
