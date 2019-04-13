30df67b87dcd3ce260cb89cb4f26c62ceff5a8f5
AhsanFaheem <ahsanfaheem1234@gmail.com>
2019-04-13 21:34:00 +0500

    first view

--- app/src/main/java/com/example/ahsan/manyconnects/NewMsg.java
+++ app/src/main/java/com/example/ahsan/manyconnects/NewMsg.java
@@ -1,17 +1,10 @@
 package com.example.ahsan.manyconnects;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import android.view.View;
 
 public class NewMsg extends AppCompatActivity {
 
@@ -19,22 +12,12 @@ public class NewMsg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_msg);
-        printHashKey(this);
     }
-
-    public void printHashKey(Context pContext) {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String hashKey = new String(Base64.encode(md.digest(), 0));
-                Log.i("RESPONSE=", "printHashKey() Hash Key: " + hashKey);
-            }
-        } catch (NoSuchAlgorithmException e) {
-            Log.e("RESPONSE=", "printHashKey()", e);
-        } catch (Exception e) {
-            Log.e("RESPONSE=", "printHashKey()", e);
-        }
+    public void checkForMediumClick(View v){
+//        if(v.getBackground().getC == getResources().getColor(R.color.colorPrimary))
+        if(ColorStateList.valueOf(getResources().getColor(R.color.colorPrimary)) != v.getBackgroundTintList())
+            v.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorPrimary)));
+        else
+            v.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
     }
 }
